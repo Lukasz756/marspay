2ec1dd073d191e2795ad0eb1715e89a608047775
Lukasz756 <jungle756@gmail.com>
2026-08-25 18:31:15 +0200

    Bootstrap Spring Boot Application

---
+++ apps/mars-payment-node/src/main/java/io/github/lukasz756/marspay/marsnode/MarsPaymentNodeApplication.java
@@ -0,0 +1,13 @@
+package io.github.lukasz756.marspay.marsnode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class MarsPaymentNodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MarsPaymentNodeApplication.class, args);
+    }
+}
